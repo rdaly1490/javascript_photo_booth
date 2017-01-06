2a8ec2ea4f762da13f3782c718613f75c10a564c
Rob <rdaly1490@gmail.com>
2017-01-05 19:21:26 -0600

    can take photo, need to add it to photo feed/photo strip

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const snap = document.querySelector('.snap');
 const takePhotoButton = document.querySelector('#take-photo');
 
 function takePhoto() {
-	console.log('click');
+	// play camera click sound
+	snap.currentTime = 0;
+	snap.play();
+
+	const base64Photo = canvas.toDataURL('image/jpeg');
+	console.log(base64Photo);
 }
 
 function getVideo() {
@@ -21,5 +26,22 @@ function getVideo() {
 			alert('We need to access your webcam for this to work...duh');
 		});
 }
+
+function paintToCanvas() {
+	// canvas and video el dimensions need to be the same
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	canvas.width = width;
+	canvas.height = height;
+
+	// every so often draw the current image to the canvas
+	// return interval in case I need to clear it later
+	return setInterval(() => {
+		ctx.drawImage(video, 0, 0, width, height);
+	}, 16);
+}
+
 getVideo();
+
 takePhotoButton.addEventListener('click', takePhoto);
+video.addEventListener('canplay', paintToCanvas);
